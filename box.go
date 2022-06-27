60b5a565e438b30867105d2973f7fdf07b942cc0
Mikhail Viaryha <mishutkavlesu@gmail.com>
2022-06-27 12:05:06 +0300

    feat: implement AddShape

--- box.go
+++ box.go
@@ -1,5 +1,6 @@
 package golang_united_school_homework
 
+import "errors"
 
 // box contains list of shapes and able to perform operations on them
 type box struct {
@@ -17,7 +18,12 @@ func NewBox(shapesCapacity int) *box {
 // AddShape adds shape to the box
 // returns the error in case it goes out of the shapesCapacity range.
 func (b *box) AddShape(shape Shape) error {
-	panic("implement me")
+	if len(b.shapes)+1 > b.shapesCapacity {
+		return errors.New("error")
+	}
+
+	b.shapes = append(b.shapes, shape)
+	return nil
 }
 
 // GetByIndex allows getting shape by index
